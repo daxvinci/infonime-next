f2ef03e648a8816361dcbf3c292fb643bdea09e2
daxvinci <ebukaokoro40@yahoo.com>
2025-05-07 13:18:29 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ThemeContextProvider from "./ThemeContextProvider";
+import Script from "next/script";
 
+
+const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,15 +32,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex flex-col antialiased`}
       >
         <ThemeContextProvider>
-          <Navbar/>
-          <main className="flex-1 flex">
-            {children}
-          </main>
-          <Footer/>
+          <Navbar />
+          <main className="flex-1 flex">{children}</main>
+          <Footer />
         </ThemeContextProvider>
       </body>
     </html>
