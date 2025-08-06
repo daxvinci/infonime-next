bcdc50b33a250eced53c13bb0625305734f28bf6
daxvinci <ebukaokoro40@yahoo.com>
2025-08-06 18:58:14 +0100

    fix responsive landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -9,16 +9,19 @@ const LandingPage = () => {
       <>
         <main className="landing-page min-h-screen relative">
           <div className="bg-wallpaper absolute inset-0 z-10" />
-          <div className="bg-overlay bg-black/40 z-20 absolute inset-0 " />
+          <div className="bg-overlay bg-black/50 z-20 absolute inset-0 " />
           <div className="content-wrapper relative z-30 flex flex-col p-4 sm:items-baseline items-center justify-center min-h-screen">
-            <div className="content flex flex-col gap-2 mt-0 sm:mt-20 ml-0 sm:ml-26 justify-center">
-              <h1 className="text-4xl text-white font-bold">
+            <div className="content max-w-[90%] sm:max-w-[40%] flex flex-col gap-2 mt-0 sm:mt-30 ml-0 sm:ml-26 justify-center">
+              <h1 className="sm:text-5xl text-2xl text-gray-200 font-bold">
                 Welcome to Infonime!
               </h1>
-              <p className="text-lg text-white mt-4">
-                A continuously updated anime list.
+              <p className="sm:text-xl text-md text-gray-300 mt-4">
+                From the latest releases to timeless favorites, we bring you a
+                constantly updated anime list that keeps your watchlist fresh.
+                Whether you're new or a longtime fan, there's always something
+                worth checking out.
               </p>
-              <div className="mt-8 flex justify-between gap-2">
+              <div className="mt-8 flex sm:gap-16 gap-8">
                 <Link href="/login" className="btn btn-primary">
                   <Button text="Sign In" color="#48C78E" />
                 </Link>
